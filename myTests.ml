a02b9379b4a0235e7eb59aef1f3e7ff6499c00d2
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2019-10-04 09:43:00 -0700

    Fixed the myTests.ml comment

--- myTests.ml
+++ myTests.ml
@@ -1,37 +1,46 @@
-(*
- *  Fill in `myTestList` with your own tests. There are two ways to add a test:
+open Runner
+open Expr
+open Printf
+open OUnit2
+
+(* Fill in `myTestList` with your own tests. There are two ways to add a test:
  *
- *  (1) By adding the code of the program as a string. In this case add an entry
- *      of this form in `myTestList`:
+ * (1) By adding the code of the program as a string. In this case, add an entry
+ *     of this form in `myTestList`:
  *
- *      (Code (<name>, <program>), <result>)
+ *     t <test_name> <program_code> <result>
  *
- *      For example:
+ * (2) By adding a test inside the 'input/' folder and adding an entry for it
+ *     in `myTestList`. The entry in this case should be:
  *
- *      (Code ("Additions", "5 + (10 + 20)"), "35")
+ *     t_file <test_name> <file_name> <result>
+ *     
+ *     Where name is the name of the file inside 'input/' with the extension
+ *     ".ana". For example:
  *
- *  (2) By adding a test inside the 'input/' folder and adding an entry for it
- *      in `myTestList`. The entry in this case should be:
- *
- *      (File <name>, <result>)
- *
- *      Where name is the name of the file inside 'input/' with the extension
- *      ".ana". For example:
- *
- *      (File "mytest.ana", "6");
- *
- *      Use a semicolon to separate your individual tests.
+ *     t_file "myTest" "mytest.ana" "6";
  *)
 
+let f_to_s fname = Runner.string_of_file ("input/" ^ fname)
+
+let t name program expected = name>::test_run program name expected;;
+let t_file name program expected = (t name (f_to_s program) expected);;
+
+let t_parse name program expected =
+  name>::(fun _ -> assert_equal expected (Runner.parse_string program));;
+
+(* For folding *)
+let t_f test_type (name,program,expected) =
+  test_type name program expected
+
+let t_err name program expected =
+  name>::test_err program name expected
+
+let t_parse_err name program expected =
+  name>::test_parse_err program name expected
+
+
 let myTestList =
- [ (* Fill in your tests here: *)
-
- ]
-;;
-
-(*
-let testFailList =
-  [
+  [ (* Fill in your tests here: *)
   ]
 ;;
-*)
